3c3999a44c1a02ddc1256570df17aff97165d1d6
Jingyu Xiang <jxiang2@ualberta.ca>
2021-12-15 23:42:37 -0700

    add create tasks, tasklists and attachments logics

--- backend/task/serializers.py
+++ backend/task/serializers.py
@@ -1,3 +1,4 @@
+from re import L
 from rest_framework import serializers
 from rest_framework.relations import HyperlinkedRelatedField
 from .models import Task, TaskList, Attachment
@@ -10,6 +11,20 @@ class TaskSerializer(serializers.ModelSerializer):
     group = serializers.CharField(source="get_group", read_only=True)
     attachments = serializers.HyperlinkedRelatedField(read_only=True, many=True, view_name="attachment-detail")
     
+    # only validate task_list
+    def validate_task_list(self, value):
+        user_profile = self.context['request'].user.profile
+        if value not in user_profile.group.task_lists.all():
+            raise serializers.ValidationError({"detail": "task list provided does not belong to group which user is in"})
+        return value
+    # do all validations before using validated data
+    def create(self, validated_data):
+        user_profile = self.context['request'].user.profile
+        task = Task.objects.create(**validated_data)
+        task.created_by = user_profile
+        task.save()
+        return task
+            
     class Meta:
         model = Task
         fields = ['url', 'id', 'name', 'description', 'task_status', 'group',
@@ -22,6 +37,26 @@ class TaskListSerializer(serializers.ModelSerializer):
     created_by = serializers.HyperlinkedRelatedField(read_only=True, many=False, view_name="profile-detail")
     tasks = HyperlinkedRelatedField(read_only=True, many=True, view_name='task-detail')
     
+    # only a group member can create task list
+    def validate(self, attrs):
+        user_profile = self.context['request'].user.profile
+        group = attrs['group']
+        if (user_profile.group) == None or (user_profile.group != group):
+            raise serializers.ValidationError({"detail" : "you can only create task list for your own group"})
+        return attrs
+    
+    def create(self, validated_data):
+        user_profile = self.context['request'].user.profile
+        if (user_profile.group) == None or (user_profile.group != validated_data['group']):
+            raise serializers.ValidationError({"detail" : "you can only create task list for your own group"})
+        else:
+            task_list = TaskList.objects.create(**validated_data)
+            task_list.created_by = user_profile
+            task_list.save()
+        return task_list
+    
+        
+    
     class Meta:
         model = TaskList
         fields = ['url', 'id', 'name', 'description', 'status', 'created_on', 'created_by', 'group', 'tasks']
@@ -29,6 +64,15 @@ class TaskListSerializer(serializers.ModelSerializer):
         
 class AttachmentSerializer(serializers.ModelSerializer):
     task = HyperlinkedRelatedField(queryset=Task.objects.all(), many=False, view_name="task-detail")
+    
+    # validate the task
+    def validate(self, attrs):
+        user_profile = self.context['request'].user.profile
+        task = attrs['task']
+        task_list = TaskList.objects.get(tasks__id__exact=task.id)
+        if task_list not in user_profile.group.task_lists.all():
+            raise serializers.ValidationError({"detail" : "task provided does not belong to group which user is in"})
+        return attrs
     
     class Meta:
         model = Attachment
