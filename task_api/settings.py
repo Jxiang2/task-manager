a998247e7980e886bc98ae5c1137e14e04b9b97c
Jingyu Xiang <jxiang2@ualberta.ca>
2021-12-16 17:13:15 -0700

    add statics

--- task_api/settings.py
+++ task_api/settings.py
@@ -168,9 +168,6 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Simplified static file serving.
